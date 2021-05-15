386613d9fc26b2a323c26ebd07b1e914e7a5f454
Gustavo Papini <gusttavopapini@gmail.com>
2021-05-14 22:47:31 -0300

    ação no botão logout

--- android/app/src/main/java/com/example/projeto_final_android_locadora/MenuGeral.java
+++ android/app/src/main/java/com/example/projeto_final_android_locadora/MenuGeral.java
@@ -2,7 +2,9 @@ package com.example.projeto_final_android_locadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuGeral extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MenuGeral extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_geral);
     }
+
+    public void fazerLogout(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
+
